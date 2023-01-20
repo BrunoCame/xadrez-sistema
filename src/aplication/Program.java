1754167ba3dcf8e485fc82b4fafc09203f80884a
Bruno Cabral <brunocabral9@hotmail.com>
2023-01-20 14:33:20 -0300

    Criado tabuleiro e peças

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,15 +1,12 @@
 package aplication;
 
-import boardgame.Posicao;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-			Posicao p = new Posicao(3, 5);
 			
-			System.out.println(p);
-
 	}
 
 }
